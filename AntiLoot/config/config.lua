a8d9591e9b79bbc9d1b1f3bc725633e241c4f88b
juhu9999 <lucas.vincek2011@gmail.com>
2025-05-04 12:33:11 +0200

    Update config.lua

--- AntiLoot/config/config.lua
+++ AntiLoot/config/config.lua
@@ -7,7 +7,7 @@
 
 Config = {}
 
-Config.UseProtectInventory = true                 -- [[ Enable this only if you are using JG_ProtectInventory ]]
+Config.UseProtectInventory = false                 -- [[ Enable this only if you are using JG_ProtectInventory ]]
 
 Config.ItemFilter = {                             -- [[ Add here your protected items ]]
     water = true,
@@ -26,19 +26,6 @@ Config.Locale = {                                 -- [[ Your Locales for Logs , 
     NoProtectedItems = "No protected items found in Config.ItemFilter!",
     NoItemFilter = "Config.ItemFilter is not set!"
 }
---[[
-Config.Locale = {                                
-NotifyBlocked = "Nemůžeš přesunout tento item!",
-DiscordTitle = "🚫 Pokus o blokované přesunutí",
-DiscordDescription = "**%s** se pokusil přesunout **%s** do **%s**, ale bylo to zablokováno.",
-DiscordAction = "Akce",
-DiscordFooter = "Anti Loot System",
-ProtectedItemsHeader = "Chráněné itemy :",
-ProtectedItem = "[ ➜ ] ^5 %s",
-NoProtectedItems = "Nebyl nalezen žádný chráněný item v Config.ItemFilter!",
-NoItemFilter = "Config.ItemFilter není nastaven!"
-}
-]]
 
 
 
